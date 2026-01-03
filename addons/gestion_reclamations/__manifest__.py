b57486880423fa43edb259c2f74fba5c137322f9
kenza belkahla <belkahlakenza001@gmail.com>
2026-01-03 16:39:12 +0100

    Initial commit - Module Odoo Gestion des Réclamations

---
+++ addons/gestion_reclamations/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Gestion des Réclamations',
+    'version': '1.0',
+    'summary': 'Gestion des réclamations internes',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/reclamation_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ addons/gestion_reclamations/models/reclamation.py
@@ -0,0 +1,19 @@
+﻿from odoo import models, fields
+
+class Reclamation(models.Model):
+    _name = 'reclamation.ticket'
+    _description = 'Réclamation Interne'
+
+    name = fields.Char(string='Sujet', required=True)
+    description = fields.Text(string='Description détaillée')
+    date_creation = fields.Date(string='Date', default=fields.Date.today)
+    priorite = fields.Selection([
+        ('0', 'Basse'),
+        ('1', 'Moyenne'),
+        ('2', 'Haute')
+    ], string='Priorité', default='1')
+    etat = fields.Selection([
+        ('nouveau', 'Nouveau'),
+        ('cours', 'En cours'),
+        ('resolu', 'Résolu')
+    ], string='Statut', default='nouveau')
